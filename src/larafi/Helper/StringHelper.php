82e08680912ce8a2bab70526edd8a75974e9cb21
MuhammadAlfianIzzah <alfianizzah6@gmail.com>
2024-05-05 18:42:25 +0800

    init

---
+++ src/larafi/Helper/StringHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Alfianizzah\Larafi\Helper;
+
+class StringHelper
+{
+    public static function helloWorld()
+    {
+        return "hello world";
+    }
+    public static function stringToArray($string)
+    {
+        return explode(" ", $string);
+    }
+    public static function randomQuote()
+    {
+        $jsonData = file_get_contents(__DIR__ . "/../datasets/quotes.json");
+        if ($jsonData === false) {
+            echo "Error reading JSON file";
+        } else {
+            // Decode JSON data into associative array
+            $data = json_decode($jsonData, true); // Passing true as the second parameter
+
+            // Check if decoding was successful
+            if ($data === null) {
+                echo "Error decoding JSON";
+            } else {
+                $randomIndex = array_rand($data);
+
+                // Display the decoded JSON data
+                return $data[$randomIndex]["quote"];
+            }
+        }
+    }
+}
